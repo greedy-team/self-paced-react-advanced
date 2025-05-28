a2b1e3e605b9d6eb08b50e2a252273409d30c50b
Shin Jihoon <tobeabestns@naver.com>
2025-05-28 21:41:37 +0900

    [adv-2.2] refactor: 최상위 컴포넌트(main.jsx)에 RecoilRoot 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <RecoilRoot>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </RecoilRoot>
+);
