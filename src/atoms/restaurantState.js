e8610f68eee794b9a59aac5f702b9b5897313d9b
Shin Jihoon <tobeabestns@naver.com>
2025-05-29 20:19:05 +0900

    [adv-2.2] refactor: 모달 관리용 openModalState atom 생성

--- src/atoms/restaurantState.js
+++ src/atoms/restaurantState.js
@@ -1,4 +1,10 @@
-import { atom } from 'recoil';
+import { atom, selector, useRecoilCallback } from 'recoil';
+import { createRestaurant } from '../api/restaurantsApi';
+
+export const openModalState = atom({
+  key: 'openModalState',
+  default: null,
+});
 
 export const restaurantState = atom({
   key: 'restaurantState',
@@ -9,3 +15,27 @@ export const selectedCategoryState = atom({
   key: 'selectedCategoryState',
   default: '전체',
 });
+
+// export const selectedRestaurantState = atom({
+//   key: 'selectedRestaurantState',
+//   default: null,
+// })
+
+// export const filteredRestaurantState = selector({
+//   key: 'filteredRestaurantState',
+//   get: ({ get }) => {
+//     const list = get(restaurantState);
+//     const category = get(selectedCategoryState);
+//     return category === '전체'
+//       ? list
+//       : list.filter(r => r.category === category);
+//   },
+// });
+
+// export const useAddRestaurant = () =>
+//   useRecoilCallback(({ snapshot, set }) =>
+//     async (newItem) => {
+//       const created = await createRestaurant(newItem);
+//       const prev = await snapshot.getPromise(restaurantState);
+//       set(restaurantState, [...prev, created]);
+//     }, [],);
