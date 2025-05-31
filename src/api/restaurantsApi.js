ea5c723d13ab33965f6e1aabbb766c5c187a56ab
Shin Jihoon <tobeabestns@naver.com>
2025-05-31 17:09:09 +0900

    [adv-2.2] refactor: 서버 주소를 하드코딩 방식에서 상수를 사용하는 방식으로 리팩토링

--- src/api/restaurantsApi.js
+++ src/api/restaurantsApi.js
@@ -1,11 +1,13 @@
+const RESTAURANT_URL = "http://localhost:3000/restaurants";
+
 export async function fetchRestaurants() {
-    const response = await fetch("http://localhost:3000/restaurants");
+    const response = await fetch(RESTAURANT_URL);
     if (!response.ok) throw new Error("GET 실패");
     return response.json();
 }
 
 export async function createRestaurant(restaurant) {
-    const response = await fetch("http://localhost:3000/restaurants", {
+    const response = await fetch(RESTAURANT_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(restaurant),
