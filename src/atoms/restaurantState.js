cc91009a6e54bf31fbe90a48fe1dca2c1a3a5fc6
Shin Jihoon <tobeabestns@naver.com>
2025-06-01 20:11:29 +0900

    [adv-2.2] refactor: restaurantState 내에 정의된 각 상태들에 대한 설명(JSdoc) 추가

--- src/atoms/restaurantState.js
+++ src/atoms/restaurantState.js
@@ -6,21 +6,65 @@ export const modalTypeState = atom({
   default: null,
 });
 
+
+/**
+ * 음식점 목록 상태
+ * [
+ *  {
+ *    id: string,
+ *    name: string,
+ *    category: string,
+ *    description: string
+ *  },
+ *  ...
+ * ]
+ * @type {Array<{ 
+ * id: string, 
+ * name: string, 
+ * category: string, 
+ * description: string 
+ * }>}
+ */
 export const restaurantState = atom({
   key: 'restaurantState',
   default: [],
 });
 
-export const selectedCategoryState = atom({
-  key: 'selectedCategoryState',
-  default: '전체',
-});
-
+/**
+ * 현재 선택된 음식점 상태
+ * 
+ * @type {{ 
+ * id: string, 
+ * name: string, 
+ * category: string, 
+ * description: string 
+ * } | null}
+ */
 export const selectedRestaurantState = atom({
   key: 'selectedRestaurantState',
   default: null,
 })
 
+/**
+ * 선택된 음식점 카테고리 상태
+ * 
+ * @type {"" | "전체" | "한식" | "중식" | "일식" | "양식" | "아시안" | "기타"}
+ */
+export const selectedCategoryState = atom({
+  key: 'selectedCategoryState',
+  default: '전체',
+});
+
+/**
+ * 선택된 카테고리에 따라 필터링된 음식점 목록 상태
+ *
+ * @type {Array<{ 
+ * id: string, 
+ * name: string, 
+ * category: string, 
+ * description: string 
+ * }>}
+ */
 export const filteredRestaurantState = selector({
   key: 'filteredRestaurantState',
   get: ({ get }) => {
@@ -32,6 +76,18 @@ export const filteredRestaurantState = selector({
   },
 });
 
+/**
+ * 음식점 추가 커스텀 훅
+ *
+ * 이 훅은 "createRestaurant" API를 호출해 새 음식점을 추가한 뒤,
+ * restaurantState 상태에 새 항목을 반영함.
+ *
+ * @returns {(newItem: { 
+ * name: string, 
+ * category: string, 
+ * description: string 
+ * }) => Promise<void>}
+ */
 export const useAddRestaurant = () =>
   useRecoilCallback(({ snapshot, set }) =>
     async (newItem) => {
