90e761d00d4424250622fd69d1c00f57d5564427
zldn109 <ss771009@naver.com>
2025-07-03 14:56:25 +0900

    refactor: try,catch 추가

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,15 @@ function App() {
   const setRestaurants = useSetRecoilState(restaurantsState);
 
   const fetchRestaurants = async () => {
-    const response = await fetch("http://localhost:3000/restaurants");
-    const data = await response.json();
-    setRestaurants(data);
+    try {
+      const response = await fetch("http://localhost:3000/restaurants");
+      if (!response.ok)
+        throw new Error("레스토랑 목록을 불러오는데 문제가 발생했습니다.");
+      const data = await response.json();
+      setRestaurants(data);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   useEffect(() => {
@@ -29,13 +35,20 @@ function App() {
   }, []);
 
   const addNewRestaurant = async (restaurant) => {
-    const response = await fetch("http://localhost:3000/restaurants", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(restaurant),
-    });
-    const newRestaurant = await response.json();
-    return newRestaurant;
+    try {
+      const response = await fetch("http://localhost:3000/restaurants", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(restaurant),
+      });
+      if (!response.ok) {
+        throw new Error("레스토랑 추가에 문제가 발생했습니다.");
+      }
+      const newRestaurant = await response.json();
+      return newRestaurant;
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const handleUpdatedRestaurants = async (restaurant) => {
