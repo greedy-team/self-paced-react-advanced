4ddd5b0c4894aa35430d8afb4b0c8ed8959a9650
gxuoo <rbdud189@naver.com>
2025-07-07 02:08:57 +0900

    refactor: redux.js 파일에 reducer import

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "../redux/rootReducer";
+import categorySlice from "../redux/categorySlice.js";
+import modalSlice from "../redux/modalSlice.js";
+import selectedRestaurantItemSlice from "../redux/selectedRestaurantItemSlice.js";
+import restaurantsSlice from "../redux/restaurantsSlice.js";
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    modal: modalSlice,
+    selectedRestaurantItem: selectedRestaurantItemSlice,
+    category: categorySlice,
+    restaurants: restaurantsSlice,
+  },
 });
