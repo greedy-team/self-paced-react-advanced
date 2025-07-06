5709efd03d1ff07f7207613766b055a5402a6b99
ChangwooJ <wjdckddn99@naver.com>
2025-07-07 00:57:16 +0900

    refactor: api 인스턴스 생성

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 export const fetchRestaurants = async () => {
-  const response = await axios.get('http://localhost:3000/restaurants');
-  return response.data;
+  const { data } = await instance.get('/restaurants');
+  return data;
 };
 
 export const addRestaurant = async (newRestaurant) => {
   try {
-    const response = await axios.post('http://localhost:3000/restaurants', newRestaurant, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    return response.data;
+    const { data } = await instance.post('/restaurants', newRestaurant);
+    return data;
   } catch (error) {
     console.error('레스토랑 추가 실패:', error);
     throw error;
