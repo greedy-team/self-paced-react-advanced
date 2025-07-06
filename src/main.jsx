ce28ffead9a0b234e97d0fe846dca980ac9273db
Shin Jihoon <tobeabestns@naver.com>
2025-07-07 00:55:14 +0900

    refactor: <StrictMode> 태그 추가

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import store from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <StrictMode>
+      <App />
+    </StrictMode>
   </Provider>
 );
