b9ea1ba223140624aeb6a758b4a87499bb844489
zldn109 <ss771009@naver.com>
2025-07-08 01:44:12 +0900

    refactor: 불필요한 주석 제거

--- src/store/RestaurantSlice.js
+++ src/store/RestaurantSlice.js
@@ -13,23 +13,6 @@ export const fetchRestaurants = createAsyncThunk(
   }
 );
 
-// const addNewRestaurant = async (restaurant) => {
-//     try {
-//       const response = await fetch("http://localhost:3000/restaurants", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(restaurant),
-//       });
-//       if (!response.ok) {
-//         throw new Error("레스토랑 추가에 문제가 발생했습니다.");
-//       }
-//       const newRestaurant = await response.json();
-//       return newRestaurant;
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
 export const addNewRestaurant = createAsyncThunk(
   "restaurants/addOne",
   async (restaurant, { rejectWithValue }) => {
