f4146ea03387210bd203d4f1acfcd615ecc8266a
INSANE-P <chanbin0626@gmail.com>
2025-10-25 20:57:15 +0900

    chore: 사용하지 않는 RTK 파일 제거

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import modalReducer from './features/modalSlice';
-import restaurantReducer from './features/restaurantSlice';
-
-export const store = configureStore({
-  reducer: { modal: modalReducer, restaurant: restaurantReducer },
-});
