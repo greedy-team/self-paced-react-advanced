d4796306157f65cdfed20a14150e1a6a955e3382
INSANE-P <chanbin0626@gmail.com>
2025-10-27 01:08:30 +0900

    feat: 쿼리의 오류 처리를 위한 오류 발생 로직 추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,22 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getRestaurants = async () => {
   const response = await fetch(`${BASE_URL}/restaurants`);
+  if (!response.ok) {
+    throw new Error('식당 불러오기 실패');
+  }
   const data = await response.json();
   return data;
 };
 
 export const addNewRestaurant = async (newRestaurant) => {
-  await fetch(`${BASE_URL}/restaurants`, {
+  const response = await fetch(`${BASE_URL}/restaurants`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(newRestaurant),
   });
+  if (!response.ok) {
+    throw new Error('식당 등록 실패');
+  }
 };
