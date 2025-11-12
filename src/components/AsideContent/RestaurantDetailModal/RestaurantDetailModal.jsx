8e1d2cf027a48fcc3c6b6a354dadb48f142fee24
dkr-sjr <rkdrjs4637@naver.com>
2025-11-12 17:04:28 +0900

    refactor(RestaurantDetailModal): styled-component 적용

--- src/components/AsideContent/RestaurantDetailModal/RestaurantDetailModal.jsx
+++ src/components/AsideContent/RestaurantDetailModal/RestaurantDetailModal.jsx
@@ -1,17 +1,62 @@
-import styles from '../RestaurantModal.module.css';
+import styled from 'styled-components';
 import Modal from '../Modal/Modal';
+
+const DescriptionContainer = styled.div`
+  margin-bottom: 24px;
+`;
+
+const Description = styled.p`
+  display: -webkit-box;
+
+  padding-top: 8px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  line-clamp: 2;
+
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 44px;
+
+  margin-right: 16px;
+
+  border: none;
+  border-radius: 8px;
+
+  font-weight: 600;
+  cursor: pointer;
+
+  background: var(--primary-color);
+
+  color: var(--grey-100);
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
 
 export default function RestaurantDetailModal({ isVisible, closeModal, restaurantInfo }) {
   if (!isVisible) return null;
   return (
     <Modal onClickBackdrop={closeModal} title={restaurantInfo.name}>
-      <div className={styles.restaurantInfo}>
-        <p className={`${styles.restaurantInfoDescription} text-body`}>{restaurantInfo.description}</p>
-      </div>
+      <DescriptionContainer>
+        <Description>{restaurantInfo.description}</Description>
+      </DescriptionContainer>
 
-      <div className={styles.buttonContainer}>
-        <button type="button" className={`${styles.button} ${styles.buttonPrimary} text-caption`} onClick={closeModal}>닫기</button>
-      </div>
+      <ButtonContainer>
+        <Button type="button" onClick={closeModal}>닫기</Button>
+      </ButtonContainer>
     </Modal>
   );
 }
