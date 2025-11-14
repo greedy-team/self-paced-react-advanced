3018a1f94dcb8e3389adcb2d842336b90587f0f4
“윤재홍” <a287933@naver.com>
2025-11-14 17:27:00 +0900

    refactor(HomeHeader):HomeHeader styled_componets 적용

---
+++ src/components/Header/HomeHeader/HomeHeader.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Gnb = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+
+  padding: 0 16px;
+
+  background-color: var(--primary-color);
+`;
+
+export const GnbTitle = styled.h1`
+  color: #fcfcfd;
+`;
--- src/components/Header/HomeHeader/index.jsx
+++ src/components/Header/HomeHeader/index.jsx
@@ -1,18 +1,17 @@
-import styles from './HomeHeader.module.css';
 import Button from '../../ui/Button';
+import { Gnb, GnbTitle } from './HomeHeader.styles';
 
 function HomeHeader({ onRestaurantAddButtonClick }) {
   return (
-    <header className={styles.gnb}>
-      <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
+    <Gnb>
+      <GnbTitle className="text-title">점심 뭐 먹지</GnbTitle>
       <Button
-        className={styles.gnb__button}
         label="음식점 추가"
         onClick={onRestaurantAddButtonClick}
       >
         <img src="/src/images/add-button.png" alt="음식점 추가" />
       </Button>
-    </header>
+    </Gnb>
   );
 }
 
