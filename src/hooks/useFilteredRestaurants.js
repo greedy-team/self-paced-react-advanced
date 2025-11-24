529cd6313cb288d00eb22e9fac4369d97d67317f
Johncakes <johnnygamesover9000@gmail.com>
2025-11-24 16:50:06 +0900

    feat: add useMemo for custom hook

--- src/hooks/useFilteredRestaurants.js
+++ src/hooks/useFilteredRestaurants.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useCategoryContext } from "../hooks/useContexts.js";
 import { useRestaurantContext } from "../hooks/useContexts.js";
 import getFilteredRestaurant from "../utils/getFilteredRestaurant.js";
@@ -6,7 +7,10 @@ export default function useFilteredRestaurants() {
   const { selectedCategory } = useCategoryContext();
   const { restaurants } = useRestaurantContext();
 
-  const filtered = getFilteredRestaurant(restaurants, selectedCategory);
+  const filtered = useMemo(
+    () => getFilteredRestaurant(restaurants, selectedCategory),
+    [restaurants, selectedCategory]
+  );
 
   return filtered;
 }
