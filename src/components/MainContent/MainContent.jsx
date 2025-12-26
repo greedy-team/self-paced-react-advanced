5dab8324d2a09e14268a44ab864033ff2d5ad5a3
dkr-sjr <rkdrjs4637@naver.com>
2025-12-26 09:01:40 +0900

    faet : 카테고리 유지 기능 추가
    zustand로 category 상태를 저장하도록 바꾸고, persist를 이용해서 새로고침해도 유지하도록 변경

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import GlobalNavigationBar from './GlobalNavigationBar/GlobalNavigationBar';
 import CategoryFilter from './CategoryFilter/CategoryFilter';
 import RestaurantList from './RestaurantList/RestaurantList';
 import useRestaurantInfoListStore from '../../stores/useRestaurantInfoListStore';
 import useAddRestaurantModalStore from '../../stores/useAddRestaurantModalStore';
 import useRestaurantDetailModalStore from '../../stores/useRestaurantDetailModalStore';
+import useCategoryStore from '../../stores/useCategoryStore';
 
 export default function MainContent() {
-  const [category, setCategory] = useState('전체');
+  const { category, setCategory } = useCategoryStore();
   const { restaurantInfoList, fetchRestaurantInfoList } = useRestaurantInfoListStore();
   const { showAddRestaurantModal } = useAddRestaurantModalStore();
   const { updateClickedRestaurantID } = useRestaurantDetailModalStore();
