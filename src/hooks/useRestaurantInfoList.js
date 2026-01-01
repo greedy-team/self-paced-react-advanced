5bf091a8ecb17b54742b3c8a4e5bd66b55a43126
dkr-sjr <rkdrjs4637@naver.com>
2026-01-02 04:18:34 +0900

    feat : 낙천적 업데이트 적용
    낙천적 업데이트 코드 추가 완료

--- src/hooks/useRestaurantInfoList.js
+++ src/hooks/useRestaurantInfoList.js
@@ -5,6 +5,8 @@ export const useRestaurantInfoListQuery = () => (
   useQuery({
     queryKey: ['restaurantInfoList'],
     queryFn: getRestaurantInfoList,
+    staleTime: 1000 * 60 * 1,
+    gcTime: 1000 * 60 * 5,
   })
 );
 
@@ -13,7 +15,24 @@ export const useAddRestaurantInfoMutation = () => {
 
   return useMutation({
     mutationFn: addNewRestaurantInfo,
-    onSuccess: () => {
+
+    onMutate: async (newInfo) => {
+      await queryClient.cancelQueries({ queryKey: ['restaurantInfoList'] });
+      const previousData = queryClient.getQueryData(['restaurantInfoList']);
+
+      queryClient.setQueryData(['restaurantInfoList'], (oldData) => [...(oldData || []), newInfo]);
+
+      return { previousData };
+    },
+
+    onError: (error, newInfo, context) => {
+      if (context?.previousData) {
+        queryClient.setQueryData(['restaurantInfoList'], context.previousData);
+      }
+      alert(`${newInfo.name} 추가 실패: ${error.message}`);
+    },
+
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['restaurantInfoList'] });
     },
   });
