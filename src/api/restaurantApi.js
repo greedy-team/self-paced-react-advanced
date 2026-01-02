73fe69e50e76e4600199b28876d44c2ca8466c0b
yoonjaehong26 <yjh0202261@gmail.com>
2026-01-02 16:41:26 +0900

    feat:fetch 검증을 위한 !response.ok 조건 추가
    fetch는 데이터 응답만 오면 Success처리하므로 fetchAllRestaurants()에 !response.ok조건을 추가하여
    200 이외의 에러코드 반환하도록 변경

--- src/api/restaurantApi.js
+++ src/api/restaurantApi.js
@@ -3,6 +3,9 @@ const LOCAL_SERVER_URL = 'http://localhost:3000';
 const restaurantApi = {
   async fetchAllRestaurants() {
     const response = await fetch(`${LOCAL_SERVER_URL}/restaurants`);
+    if (!response.ok) {
+      throw new Error('데이터를 불러오는데 실패했습니다.');
+    }
     return response.json();
   },
 
@@ -12,6 +15,9 @@ const restaurantApi = {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(restaurant),
     });
+    if (!response.ok) {
+      throw new Error('데이터를 저장하는데 실패했습니다.');
+    }
     return response.json();
   },
 };
