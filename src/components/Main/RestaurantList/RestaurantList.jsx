55eb1411a2bd79e3575a2079bed77a27ffd2bd79
yoonjaehong26 <yjh0202261@gmail.com>
2026-01-05 08:59:53 +0900

    refacor:Data First 철학에 맞게 데이터를 우선확인하도록 코드 순서 변경
    데이터가 있는 지 우선확인 함으로써 undefined으로 인한 오류를 방지하고
    네트워크 불안정시 refetchOnReconnect으로 인한 에러메세지를 방지하기위하여
    데이터 존재확인-isError,isLoading확인하도록 순서변경

--- src/components/Main/RestaurantList/RestaurantList.jsx
+++ src/components/Main/RestaurantList/RestaurantList.jsx
@@ -7,26 +7,29 @@ import useRestaurantStore from '../../../stores/RestaurantStore';
 import useRestaurants from '../../../hooks/useRestaurants';
 
 function RestaurantList() {
-  const { data: fetchedRestaurants, isLoading, error } = useRestaurants();
+  const { data: fetchedRestaurants, error } = useRestaurants();
 
   const selectedCategory = useRestaurantStore((state) => state.selectedCategory);
 
-  if (isLoading) return <div>로딩 중... ⏳</div>;
+  if (fetchedRestaurants) {
+    const filteredRestaurants = selectedCategory === '전체'
+      ? fetchedRestaurants
+      : fetchedRestaurants.filter((restaurant) => restaurant.category === selectedCategory);
+
+    return (
+      <RestaurantListContainer>
+        <RestaurantItemList>
+          {filteredRestaurants.map((restaurant) => (
+            <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+          ))}
+        </RestaurantItemList>
+      </RestaurantListContainer>
+    );
+  }
+
   if (error) return <div>데이터를 가져오지 못했어요. ❌</div>;
 
-  const filteredRestaurants = selectedCategory === '전체'
-    ? fetchedRestaurants
-    : fetchedRestaurants.filter((restaurant) => restaurant.category === selectedCategory);
-
-  return (
-    <RestaurantListContainer>
-      <RestaurantItemList>
-        {filteredRestaurants.map((restaurant) => (
-          <RestaurantItem key={restaurant.id} restaurant={restaurant} />
-        ))}
-      </RestaurantItemList>
-    </RestaurantListContainer>
-  );
+  return <div>로딩 중... ⏳</div>;
 }
 
 export default RestaurantList;
