570503102745000dd201457fe92b5c8cf0418017
yoonjaehong26 <yjh0202261@gmail.com>
2026-01-05 09:57:03 +0900

    feat:식당추가 직후 모달닫기, 성공/실패후 토스트알림 추가
    낙관적 업데이트 적용에 따라 ux개선을 위해 식당추가 직후 모달을 닫도록 하고,
    유저에게 정확한 피드백을 위하여 react-toastify를 통하여 성공 및 실패 후에 토스트 알림을 추가했습니다.
    onMutate이후 토스트알림을 추가하려고 하였으나, 낙관적 업데이트에 맞는 것 같지 않아 onError와 onSuccess에만 추가했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { ToastContainer } from 'react-toastify';
 import HomeHeader from './components/Header/HomeHeader';
 import RestaurantCategoryFilter from './components/Main/RestaurantCategoryFilter';
 import RestaurantList from './components/Main/RestaurantList/RestaurantList';
 import GlobalStyle from './GlobalStyle';
 import RestaurantDetailModal from './components/Aside/RestaurantDetailModal';
 import AddRestaurantModal from './components/Aside/AddRestaurantModal';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar
+      />
       <GlobalStyle />
       <div>
         <HomeHeader />
--- src/hooks/useAddRestaurant.js
+++ src/hooks/useAddRestaurant.js
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 import restaurantApi from '../api/restaurantApi';
 import queryKeys from '../constants/queryKeys';
 
@@ -14,17 +15,17 @@ const useAddRestaurant = (handleClose) => {
 
       queryClient.setQueryData(queryKeys.restaurants.all, (prev = []) => [...prev, newRestaurant]);
 
+      handleClose();
       return { previousRestaurants: previousRestaurant };
     },
 
     onError: (err, newRestaurant, context) => {
       queryClient.setQueryData(queryKeys.restaurants.all, context.previousRestaurants);
+      toast.error(`음식점 추가에 실패했어요. 다시 시도해주세요. 😢 (${err.message})`);
     },
 
     onSuccess: () => {
-      if (handleClose) {
-        handleClose();
-      }
+      toast.success('식당이 성공적으로 추가되었습니다! 🎉');
     },
 
     onSettled: () => {
