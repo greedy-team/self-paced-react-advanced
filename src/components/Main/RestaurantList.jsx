5ec1a0a1210d7f137de8c57ddacca230d301af0d
rahwan10 <donggeon10@naver.com>
2026-05-11 22:42:51 +0900

    feat: RestaurantList styled-components 구현

--- src/components/Main/RestaurantList.jsx
+++ src/components/Main/RestaurantList.jsx
@@ -1,13 +1,12 @@
-import PropTypes from 'prop-types';
-import '../styles/default.css';
-import '../styles/RestaurantList.css';
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import korean from '../../../templates/category-korean.png';
-import chinese from '../../../templates/category-chinese.png';
-import japanese from '../../../templates/category-japanese.png';
-import western from '../../../templates/category-western.png';
-import asian from '../../../templates/category-asian.png';
-import etc from '../../../templates/category-etc.png';
+import korean from "../../../templates/category-korean.png";
+import chinese from "../../../templates/category-chinese.png";
+import japanese from "../../../templates/category-japanese.png";
+import western from "../../../templates/category-western.png";
+import asian from "../../../templates/category-asian.png";
+import etc from "../../../templates/category-etc.png";
 
 const categoryImage = {
   한식: korean,
@@ -18,9 +17,62 @@ const categoryImage = {
   기타: etc,
 };
 
+const ResaurantListContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: 0 16px;
+  margin: 16px 0;
+`;
+
+const RestuarantCategory = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 64px;
+  min-width: 64px;
+  min-height: 64px;
+
+  margin-right: 16px;
+
+  border-radius: 50%;
+  background: #f6a88a;
+`;
+
+const IconImage = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+
+const RestaurantInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const RestaurantName = styled.h3`
+  margin: 0;
+`;
+
+const RestaurantDistance = styled.p`
+  color: #ec4a0a;
+`;
+
+const RestaurantDescription = styled.p`
+  display: -webkit-box;
+
+  padding-top: 8px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
 function RestaurantList({ filteredRestaurants, handleClickRestaurantList }) {
   return (
-    <section className="restaurant-list-container">
+    <ResaurantListContainer>
       <ul className="restaurant-list">
         {filteredRestaurants.map((r) => (
           <div
@@ -34,17 +86,21 @@ function RestaurantList({ filteredRestaurants, handleClickRestaurantList }) {
             onKeyDown={() => handleClickRestaurantList(r)}
             className="restaurant"
           >
-            <div className="restaurant__category">
-              <img src={categoryImage[r.category]} alt={r.category} className="category-icon" />
-            </div>
-            <div className="restaurant__info">
-              <h3 className="restaurant__name text-subtitle">{r.name}</h3>
-              <p className="restaurant__description text-body">{r.description}</p>
-            </div>
+            <RestuarantCategory>
+              <IconImage
+                src={categoryImage[r.category]}
+                alt={r.category}
+                className="category-icon"
+              />
+            </RestuarantCategory>
+            <RestaurantInfo>
+              <RestaurantName>{r.name}</RestaurantName>
+              <RestaurantDescription>{r.description}</RestaurantDescription>
+            </RestaurantInfo>
           </div>
         ))}
       </ul>
-    </section>
+    </ResaurantListContainer>
   );
 }
 
