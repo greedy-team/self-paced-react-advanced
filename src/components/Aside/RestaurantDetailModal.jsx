1ec9440958541094ce1f50a151577084a9a1409c
rahwan10 <donggeon10@naver.com>
2026-05-11 22:49:23 +0900

    feat: RestaurantDetailModal styled-components적용

--- src/components/Aside/RestaurantDetailModal.jsx
+++ src/components/Aside/RestaurantDetailModal.jsx
@@ -1,34 +1,84 @@
-import PropTypes from 'prop-types';
-import '../styles/default.css';
-import '../styles/RestaurantDetailModal.css';
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import "../styles/default.css";
+//import '../styles/RestaurantDetailModal.css';
 
+const OpendModal = styled.div`
+  display: block;
+`;
+
+const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+
+  padding: 32px 16px;
+
+  border-radius: 8px 8px 0px 0px;
+  background: var(--grey-100);
+`;
+
+const ModalTitle = styled.h2`
+  margin-bottom: 36px;
+`;
+
+const RestaurantInfo = styled.div`
+  margin-bottom: 24px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 44px;
+
+  margin-right: 16px;
+
+  border: none;
+  border-radius: 8px;
+
+  font-weight: 600;
+  cursor: pointer;
+  background: #ec4a0a;
+
+  color: #ffffff;
+`;
 function RestaurantDetailModal({ setDetailModal, selectedRestaurant }) {
   return (
-    <div className="modal modal--open">
-      <div
+    <OpendModal>
+      <ModalBackdrop
         role="button"
         tabIndex={0}
-        className="modal-backdrop"
         aria-label="모달 닫기"
         onKeyDown={() => setDetailModal(false)}
         onClick={() => setDetailModal(false)}
       />
-      <div className="modal-container">
-        <h2 className="modal-title text-title">{selectedRestaurant.name}</h2>
-        <div className="restaurant-info">
-          <p className="restaurant-info__description text-body">{selectedRestaurant.description}</p>
-        </div>
-        <div className="button-container">
-          <button
-            type="button"
-            className="button button--primary text-caption"
-            onClick={() => setDetailModal(false)}
-          >
+      <ModalContainer>
+        <ModalTitle>{selectedRestaurant.name}</ModalTitle>
+        <RestaurantInfo>
+          <p className="restaurant-info__description text-body">
+            {selectedRestaurant.description}
+          </p>
+        </RestaurantInfo>
+        <ButtonContainer>
+          <Button type="button" onClick={() => setDetailModal(false)}>
             닫기
-          </button>
-        </div>
-      </div>
-    </div>
+          </Button>
+        </ButtonContainer>
+      </ModalContainer>
+    </OpendModal>
   );
 }
 RestaurantDetailModal.propTypes = {
