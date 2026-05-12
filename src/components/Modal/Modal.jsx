75f5fafbe80ef280e27e5f537e030307b9374097
gyuminJJANG <ggavwu789@gmail.com>
2026-05-12 16:39:11 +0900

    refactor: Modal 컴포넌트 styled-components 전환 및 CSS 파일 제거

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,35 @@
-import "./Modal.css";
+import styled from "styled-components";
 
 export default function Modal({ title, children, onClose }) {
   return (
     <>
-      <div className="modal-backdrop" onClick={onClose}></div>
-      <div className="modal-container">
-        <h2 className="modal-title text-title">{title}</h2>
+      <ModalBackdrop onClick={onClose}></ModalBackdrop>
+      <ModalContainer>
+        <ModalTitle>{title}</ModalTitle>
         {children}
-      </div>
+      </ModalContainer>
     </>
   );
 }
+const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+
+  padding: 32px 16px;
+
+  border-radius: 8px 8px 0px 0px;
+  background: var(--grey-100);
+`;
+
+const ModalTitle = styled.h2``;
