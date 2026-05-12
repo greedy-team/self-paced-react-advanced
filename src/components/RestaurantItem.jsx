753c00184292ff5a3975ccec7490437720074436
EM-H20 <ghd0106@gmail.com>
2026-05-13 00:50:34 +0900

    refactor: RestaurantItem을 styled-components로 전환

--- src/components/RestaurantItem.jsx
+++ src/components/RestaurantItem.jsx
@@ -1,10 +1,10 @@
+import styled from "styled-components";
 import categoryAsian from "../assets/category-asian.png";
 import categoryChinese from "../assets/category-chinese.png";
 import categoryEtc from "../assets/category-etc.png";
 import categoryJapanese from "../assets/category-japanese.png";
 import categoryKorean from "../assets/category-korean.png";
 import categoryWestern from "../assets/category-western.png";
-import styles from "./RestaurantItem.module.css";
 
 const categoryImages = {
   한식: categoryKorean,
@@ -15,22 +15,79 @@ const categoryImages = {
   기타: categoryEtc,
 };
 
+const Restaurant = styled.li`
+  display: flex;
+  align-items: flex-start;
+
+  padding: 16px 8px;
+
+  border-bottom: 1px solid #e9eaed;
+
+  cursor: pointer;
+`;
+
+const Category = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 64px;
+  min-width: 64px;
+  min-height: 64px;
+
+  margin-right: 16px;
+
+  border-radius: 50%;
+  background: var(--lighten-color);
+`;
+
+const CategoryIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const Name = styled.h3`
+  margin: 0;
+
+  font-size: 18px;
+  line-height: 28px;
+  font-weight: 600;
+`;
+
+const Description = styled.p`
+  display: -webkit-box;
+
+  padding-top: 8px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+`;
+
 export default function RestaurantItem({ restaurant, onClick }) {
   return (
-    <li className={styles.restaurant} onClick={onClick}>
-      <div className={styles.category}>
-        <img
+    <Restaurant onClick={onClick}>
+      <Category>
+        <CategoryIcon
           src={categoryImages[restaurant.category]}
           alt={restaurant.category}
-          className={styles.categoryIcon}
         />
-      </div>
-      <div className={styles.info}>
-        <h3 className={`${styles.name} text-subtitle`}>{restaurant.name}</h3>
-        <p className={`${styles.description} text-body`}>
-          {restaurant.description}
-        </p>
-      </div>
-    </li>
+      </Category>
+      <Info>
+        <Name>{restaurant.name}</Name>
+        <Description>{restaurant.description}</Description>
+      </Info>
+    </Restaurant>
   );
 }
