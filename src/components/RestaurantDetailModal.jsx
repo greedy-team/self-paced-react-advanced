ace1c8aa3c7104ad786c3c6a64db5258008abb92
EM-H20 <ghd0106@gmail.com>
2026-05-13 00:50:46 +0900

    refactor: RestaurantDetailModal을 styled-components로 전환

--- src/components/RestaurantDetailModal.jsx
+++ src/components/RestaurantDetailModal.jsx
@@ -1,24 +1,100 @@
-import styles from "./RestaurantDetailModal.module.css";
+import styled from "styled-components";
+
+const Modal = styled.div`
+  display: none;
+`;
+
+const ModalOpen = styled(Modal)`
+  display: block;
+`;
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+
+  padding: 32px 16px;
+
+  border-radius: 8px 8px 0px 0px;
+  background: var(--grey-100);
+`;
+
+const Title = styled.h2`
+  margin-bottom: 36px;
+
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-bottom: 24px;
+`;
+
+const Description = styled.p`
+  display: -webkit-box;
+
+  padding-top: 8px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 44px;
+
+  margin-right: 16px;
+
+  border: none;
+  border-radius: 8px;
+
+  font-weight: 600;
+  cursor: pointer;
+
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+const PrimaryButton = styled(Button)`
+  background: var(--primary-color);
+
+  color: var(--grey-100);
+`;
+
 export default function RestaurantDetailModal({ restaurant, onClose }) {
   return (
-    <div className={`${styles.modal} ${styles.open}`}>
-      <div className={styles.backdrop} onClick={onClose}></div>
-      <div className={styles.container}>
-        <h2 className={`${styles.title} text-title`}>{restaurant.name}</h2>
-        <div className={styles.info}>
-          <p className={`${styles.description} text-body`}>
-            {restaurant.description}
-          </p>
-        </div>
-        <div className={styles.buttonContainer}>
-          <button
-            className={`${styles.button} ${styles.primary} text-caption`}
-            onClick={onClose}
-          >
-            닫기
-          </button>
-        </div>
-      </div>
-    </div>
+    <ModalOpen>
+      <Backdrop onClick={onClose} />
+      <Container>
+        <Title>{restaurant.name}</Title>
+        <Info>
+          <Description>{restaurant.description}</Description>
+        </Info>
+        <ButtonContainer>
+          <PrimaryButton onClick={onClose}>닫기</PrimaryButton>
+        </ButtonContainer>
+      </Container>
+    </ModalOpen>
   );
 }
