6653f167555dbe264ddc926f2e90aa93d6216eb8
gyuminJJANG <ggavwu789@gmail.com>
2026-05-12 16:35:40 +0900

    refactor: RestaurantList 컴포넌트 styled-components 전환 및 CSS 파일 제거

--- src/components/RestaurantList/RestaurantItem.jsx
+++ src/components/RestaurantList/RestaurantItem.jsx
@@ -1,21 +1,70 @@
-import { CATEGORY_IMAGE } from "../../RestaurantData"
+import styled from "styled-components";
+import { CATEGORY_IMAGE } from "../../RestaurantData";
 
-export default function RestaurantItem({item, onOpenModal}){
-    return(
-        <li className="restaurant" onClick={()=>onOpenModal(item)}>
-            <div className="restaurant__category">
-                <img
-                    src={CATEGORY_IMAGE[item.category]}
-                    alt={item.category}
-                    className="category-icon"
-                />
-            </div>
-            <div className="restaurant__info">
-                <h3 className="restaurant__name text-subtitle">{item.name}</h3>
-                <p className="restaurant__description text-body">
-                    {item.description}
-                </p>
-            </div>
-        </li>
-    );
+export default function RestaurantItem({ item, onOpenModal }) {
+  return (
+    <RestaurantLi onClick={() => onOpenModal(item)}>
+      <RestaurantCategory>
+        <CategoryIcon src={CATEGORY_IMAGE[item.category]} alt={item.category} />
+      </RestaurantCategory>
+      <RestaurantInfo>
+        <RestaurantName>{item.name}</RestaurantName>
+        <Description>{item.description}</Description>
+      </RestaurantInfo>
+    </RestaurantLi>
+  );
 }
+
+const RestaurantLi = styled.li`
+  display: flex;
+  align-items: flex-start;
+
+  padding: 16px 8px;
+
+  border-bottom: 1px solid #e9eaed;
+`;
+
+const RestaurantCategory = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 64px;
+  min-width: 64px;
+  min-height: 64px;
+
+  margin-right: 16px;
+
+  border-radius: 50%;
+  background: var(--lighten-color);
+`;
+
+const CategoryIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+
+const RestaurantInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const RestaurantName = styled.h3`
+  margin: 0;
+`;
+
+const Description = styled.p`
+  display: -webkit-box;
+
+  padding-top: 8px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+const RestaurantDistance = styled.span`
+  color: var(--primary-color);
+`;
--- src/components/RestaurantList/RestaurantList.jsx
+++ src/components/RestaurantList/RestaurantList.jsx
@@ -1,18 +1,24 @@
-import "./RestaurantList.css"
-import RestaurantItem from "./RestaurantItem"
+import styled from "styled-components";
+import RestaurantItem from "./RestaurantItem";
 
-export default function RestaurantList({restaurants, onOpenModal}){
-    return(
-        <section className="restaurant-list-container">
-            <ul className="restaurant-list">
-                {restaurants.map((item) => (
-                    <RestaurantItem
-                    key={item.id}
-                    item={item}
-                    onOpenModal={onOpenModal}
-                    />
-                ))}
-            </ul>
-        </section>
-    )
+export default function RestaurantList({ restaurants, onOpenModal }) {
+  return (
+    <RestaurantListContainer>
+      <RestaurantListUl>
+        {restaurants.map((item) => (
+          <RestaurantItem key={item.id} item={item} onOpenModal={onOpenModal} />
+        ))}
+      </RestaurantListUl>
+    </RestaurantListContainer>
+  );
 }
+
+const RestaurantListContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: 0 16px;
+  margin: 16px 0;
+`;
+
+const RestaurantListUl = styled.ul``;
