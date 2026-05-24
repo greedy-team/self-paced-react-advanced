982630946a78d6e9cc4963ee51c59ca4a412ff8a
rahwan10 <donggeon10@naver.com>
2026-05-24 21:24:56 +0900

    feat: 기본값 추가로 오류방지

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,7 +1,10 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 
-export const UserContext = createContext();
+export const UserContext = createContext({
+  category: "전체",
+  setCategory: () => {},
+});
 
 export function UserProvider({ children }) {
   const [category, setCategory] = useState("전체");
