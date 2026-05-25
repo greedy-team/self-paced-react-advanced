c2521b2ae67bedd2761c20b743e4eba37e7d3415
EM-H20 <ghd0106@gmail.com>
2026-05-26 00:12:58 +0900

    feat: ModalContext,ModalProvider 생성

---
+++ src/contexts/ModalProvider.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { ModalContext } from "./ModalContext";
+
+export default function ModalProvider({ children }) {
+  // App.jsx에서 관리하던 activeModal 상태를 ModalProvider로 이동
+  const [activeModal, setActiveModal] = useState(null);
+
+  const value = {
+    activeModal,
+    setActiveModal,
+  };
+
+  return (
+    <ModalContext.Provider value={value}>{children}</ModalContext.Provider>
+  );
+}
